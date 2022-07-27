aaa4e898fcf5958fca3bd54f789158fb8c686aac
Sameer Jain <72691195+Sameer4522@users.noreply.github.com>
2022-07-27 14:08:25 +0530

    first commit

---
+++ src/components/weather/weatherCard.js
@@ -0,0 +1,124 @@
+import React, { useEffect } from "react";
+
+const WeatherCard = ({ tempInfo }) => {
+  const {
+    temp,
+    pressure,
+    humidity,
+    weathermood,
+    speed,
+    name,
+    country,
+    sunset,
+  } = tempInfo;
+
+  // converting time
+
+  let seconds = sunset;
+  let date = new Date(seconds * 1000);
+  // converted time into mili seconds
+
+  let time = `${date.getHours()}:${date.getMinutes()}`;
+
+  const [weatherState, setWeatheState] = React.useState("");
+
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setWeatheState("wi-day-cloudy");
+          break;
+        case "Haze":
+          setWeatheState("wi-fog");
+          break;
+        case "Clear":
+          setWeatheState("wi-day-sunny");
+          break;
+        case "Mist":
+          setWeatheState("wi-dust");
+          break;
+
+        default:
+          setWeatheState("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weathermood]);
+
+  return (
+    <>
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={`wi ${weatherState}`}></i>
+        </div>
+
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>{temp}&deg;</span>
+          </div>
+
+          <div className="description">
+            <div className="weatherCondition">{weathermood}</div>
+            <div className="place">
+              {name}, {country}
+            </div>
+          </div>
+        </div>
+
+        <div className="date"> {new Date().toLocaleString()}</div>
+
+        {/* weather data section */}
+
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {time} PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {humidity}
+                <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {pressure}
+                <br />
+                Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {speed} <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default WeatherCard;
